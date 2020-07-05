7d1096f0eef2019d9d4080c601f98469cfb1a9a6
Jina Strickland <jina30@gmail.com>
2020-07-05 16:47:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,23 +1,16 @@
 require 'pry'
 require "yaml"
 
-# def load_library(file_path)
-#   emoticons = YAML.load_file(file_path)
-#   binding.pry
-#   p file_path
-#  end
-
-
-
 def load_library(file_path)
   revised_emoticons = Hash.new
+  binding.pry
   emoticons_list = YAML.load_file(file_path)
   emoticons_list.each do |key, value|
     revised_emoticons[key] = {}
       revised_emoticons[key][:english] = value[0]
       revised_emoticons[key][:japanese] = value[1]
-      end
-      revised_emoticons
+    end
+  revised_emoticons
 end
 
 
@@ -27,14 +20,6 @@ end
 #     :japanese => value[1]
 #     }
 #   }
-
-# value.each do |name, emoticons|
-# binding.pry
-#   emoticons.each do |emoticon|
-
-# file.map { |k, v| [k, v]}.to_h
-# hash = hash[array.collect { |item| [item, "value"]}]
-
 
 
 def get_japanese_emoticon
