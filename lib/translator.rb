7eb1442231a392a40667bf307efbda1bb8fb8625
Jina Strickland <jina30@gmail.com>
2020-07-05 17:27:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,6 +16,7 @@ end
 def get_english_meaning(file_path, emoticon)
   thelist = load_library(file_path)
   thelist.each do |k, v|
+      binding.pry
     thelist[k]
   binding.pry
   # the_list.each do |k, v|
