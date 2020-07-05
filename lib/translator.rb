6d11e2c17f32efd198c4d8774f4e565e9e90e469
Jina Strickland <jina30@gmail.com>
2020-07-05 14:46:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,7 +10,7 @@ require "yaml"
 def load_library(file_path)
  emoticons = YAML.load_file(file_path)
   emoticons.each_with_object({}) do |(key, value), final_array|
-    value.each do |inner_key, inner_value|
+    
 binding.pry
   p file_path
   final_array
