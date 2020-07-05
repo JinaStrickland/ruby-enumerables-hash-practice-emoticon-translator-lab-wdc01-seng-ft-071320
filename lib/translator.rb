17d043e75510a9f48d90c77ca587831d74956127
Jina Strickland <jina30@gmail.com>
2020-07-05 16:10:37 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,13 +13,13 @@ def load_library(file_path)
   revised_emoticons = {}
   emoticons_list = YAML.load_file(file_path)
   emoticons_list.each do |key, value|
-    revised_emoticons = {
+      {
         key =>  {
           :english => value[0],
           :japanese => value[1]
           }
         }
-
+      revised_emoticons = emoticons_list
     #binding.pry
   end
 end
